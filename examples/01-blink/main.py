395cd5ce94d6c2162f7060c185a9eabf96394284
tomas-fryza <tomas.fryza@gmail.com>
2023-06-16 10:31:11 +0200

    Remove comments

--- examples/01-blink/main.py
+++ examples/01-blink/main.py
@@ -3,32 +3,26 @@
 A simple example of how to control one output pin of 
 a microcontroller. An LED is connected to the GPIO pin,
 which is repeatedly switched on and off.
+
+See also:
+    https://wokwi.com/projects/359801682833812481
 """
 
 from machine import Pin
-from time import sleep_ms
+from utime import sleep_ms
 
 # Check the LED pin on your board, usually it is GPIO2
 PIN_LED = 2
 
+print("Configure output pin...", end="")
+led = Pin(PIN_LED, Pin.OUT)
+print("Done")
 
-def main():
-    print("Configure output pin...", end="")
-    led = Pin(PIN_LED, Pin.OUT)
-    print("Done")
-    print(f"Start blinking pin {PIN_LED}")
+print(f"Start blinking pin {PIN_LED}")
 
-    # Forever loop
-    while True:
-        led.on()
-        sleep_ms(750)
-        led.off()
-        sleep_ms(250)
-
-"""Top-level code is executed when the program is run directly
-by the Python interpreter. Note that the code inside the if
-statement is not executed when the file's code is imported
-as a module.
-"""
-if __name__ == "__main__":
-    main()
+# Forever loop
+while True:
+    led.on()
+    sleep_ms(125)
+    led.off()
+    sleep_ms(875)
