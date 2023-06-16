eeb629a9879d95928e81249ce29a0963b6f4a088
tomas-fryza <tomas.fryza@gmail.com>
2023-06-16 10:31:48 +0200

    Wi-Fi scan example created

---
+++ examples/03-wifi_scan/main.py
@@ -0,0 +1,40 @@
+"""Scan Wi-Fi access points.
+
+Activate the WLAN interface and scan for nearby access points (AP).
+Display  the main parameters of APs.
+
+See also:
+    https://wokwi.com/projects/305570169692881473
+    https://github.com/micropython/micropython/issues/10017
+"""
+
+import network
+import ubinascii
+
+# Initialize wlan object
+wlan = network.WLAN(network.STA_IF)
+# Activate wlan interface
+wlan.active(True)
+
+
+def scan():
+    print("Scanning for Wi-Fi networks, please wait...", end="")
+
+    # Perform a Wi-Fi APs scan
+    accessPoints = wlan.scan()
+
+    print("Done")
+    print("")
+
+    print("SSID             | MAC               | Ch.| RSSI| AuthMode")
+    print("-----------------+-------------------+----+-----+------------")
+
+    authmodes = ["Open", "WEP", "WPA-PSK", "WPA2-PSK4", "WPA/WPA2-PSK", "other"]
+
+    # Print each AP found in a single row
+    for (ssid, bssidap, channel, rssi, authmode, vis) in accessPoints:
+        bssid =  ubinascii.hexlify(bssidap,":").decode()
+        print(f"{ssid:16s} | {bssid:s} | {channel:2d} | {rssi:3d} | {authmodes[authmode]:s}")
+
+# Call scanning function
+scan()
