f64ae8b35ba52146532d265b4c18ab7f433196dc
tomas-fryza <tomas.fryza@gmail.com>
2023-08-15 13:08:19 +0200

    UART example tested

--- examples/10-uart/main.py
+++ examples/10-uart/main.py
@@ -1,16 +1,33 @@
-"""UART example.
+"""Example of internal UARTs.
 
-TBD.
+Two UART instances are used independently: UART0 is used
+for serial REPL and UART1 is used for serial data communication.
+Note that, only serial transmitters are used here.
 
 Inspired by:
-     TBD
+    * https://forum.micropython.org/viewtopic.php?t=6076
+    * https://github.com/loboris/MicroPython_ESP32_psRAM_LoBo/wiki/uart
 """
 
+from machine import Pin
 from machine import UART
+from time import sleep_ms
 
-uart = UART(0, baudrate=115200, tx=1, rx=3)
+# Status LED
+led = Pin(2, Pin.OUT)
 
-uart.write("test")
+# Create two UART instances, one for REPL (TX=1, RX=3) and one
+# for other serail communication
+uart0 = UART(0, tx=1, rx=3)
+uart0.init(baudrate=115200, bits=8, parity=None, stop=1)
+uart1 = UART(1, tx=25, rx=26)
+uart1.init(baudrate=9600, bits=8, parity=None, stop=1)
 
 while True:
-    pass
+    led.on()
+    # `uart0.write()` has the same output as `print()`
+    uart0.write("uart0\r\n")
+    print("Hi there")
+    uart1.write("uart1\r\n")
+    led.off()
+    sleep_ms(1000)
