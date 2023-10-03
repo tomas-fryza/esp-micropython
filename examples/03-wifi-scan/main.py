c4477ca3246cdf232db68977412d47e58e01feb7
tomas-fryza <tomas.fryza@gmail.com>
2023-10-03 11:00:56 +0200

    Wifi scan code simplified

--- examples/03-wifi-scan/main.py
+++ examples/03-wifi-scan/main.py
@@ -1,10 +1,7 @@
 """Scan Wi-Fi access points.
 
 Activate the WLAN interface and scan for nearby access points (AP).
-Display the main parameters of APs.
-
-TODOs:
-    * Add comments to all functions
+Display SSID, channel index and signal strength of APs.
 
 Inspired by:
     * https://wokwi.com/projects/305570169692881473
@@ -12,34 +9,20 @@ Inspired by:
 """
 
 import network
-import ubinascii  # Conversions between binary data and ASCII
 
-# Create Station interface
+# Initialize the WLAN (Station mode interface)
 sta_if = network.WLAN(network.STA_IF)
 # Activate station/Wi-Fi client interface
 sta_if.active(True)
 
+# Perform the Wi-Fi APs scan
+print("Scanning for Wi-Fi networks, please wait... ", end="")
+available_networks = sta_if.scan()
+print("Done")
+print("")
 
-def scan_wifi():
-    print("Scanning for Wi-Fi networks, please wait... ", end="")
-
-    # Perform a Wi-Fi APs scan
-    accessPoints = sta_if.scan()
-
-    print("Done")
-    print("")
-
-    print("SSID             | MAC               | Ch.| RSSI| AuthMode")
-    print("-----------------+-------------------+----+-----+------------")
-
-    authmodes = ["Open", "WEP", "WPA-PSK", "WPA2-PSK4",
-                 "WPA/WPA2-PSK", "other", "other", "other"]
-
-    # Print each AP params in a single row
-    for (ssid, bssidap, channel, rssi, authmode, vis) in accessPoints:
-        bssid = ubinascii.hexlify(bssidap, ":").decode()
-        print(f"{ssid:16s} | {bssid:s} | {channel:2d} | {rssi:3d} | {authmodes[authmode]:s}")
-
-
-# Call scanning function
-scan_wifi()
+# Print the list of available Wi-Fi networks
+print("SSID              | Channel | Signal Strength (dBm)")
+print("------------------+---------+----------------------")
+for network in available_networks:
+    print(f"{network[0].decode("utf-8"):17s} | {network[2]:7d} | {network[3]:10d}")
