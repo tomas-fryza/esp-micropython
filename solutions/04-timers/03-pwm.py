7fcd43e8a97460a96026378ccc61fc38f252d2e5
tomas-fryza <tomas.fryza@gmail.com>
2023-10-20 12:05:40 +0200

    pwm examples improved

---
+++ solutions/04-timers/03-pwm.py
@@ -0,0 +1,37 @@
+"""
+Pulse Width Modulation (PWM)
+
+MicroPython demonstrates the use of PWM to control the brightness
+of an LED connected to an ESP32 microcontroller. PWM allows you
+to vary the LED's brightness by changing the duty cycle of the
+PWM signal.
+
+See:
+   https://www.upesy.com/blogs/tutorials/micropython-esp32-pwm-usage
+
+Hardware Configuration:
+  - LED: GPIO pin 2 (onboard)
+
+Instructions:
+1. Use onboard LED or connect an external
+2. Run the current script
+3. Stop the code execution by pressing `Ctrl+C` key.
+   If it does not respond, press the onboard `reset` button.
+
+Author: uPesy, Tomas Fryza
+Date: 2023-10-16
+"""
+
+from machine import Pin, PWM
+import time
+
+# Define the LED pin
+led = Pin(2, Pin.OUT)
+# Attach PWM object on the LED pin and set frequency to 1 kHz
+led_with_pwm = PWM(led, freq=1000)
+
+while True:
+    for duty in range(100):  # Duty from 0 to 100 %
+        # Pulse width resolution is 10-bit only !
+        led_with_pwm.duty(int(duty/100 * 1024))
+        time.sleep_ms(15)
---
+++ solutions/04-timers/04-dimmer.py
@@ -0,0 +1,78 @@
+"""
+LED dimmer
+
+Example combines timer interrupts and PWM (Pulse Width Modulation)
+for dimming an LED on an ESP32 using MicroPython. In this example,
+a timer interrupt is used to periodically update the PWM duty cycle
+to create a fading effect on the LED.
+
+Hardware Configuration:
+  - LED: GPIO pin 2 (onboard)
+
+Instructions:
+1. Use onboard LED or connect an external
+2. Run the current script
+3. Stop the code execution by pressing `Ctrl+C` key.
+   If it does not respond, press the onboard `reset` button.
+
+Author: Tomas Fryza
+Date: 2023-10-20
+"""
+
+from machine import Pin, Timer, PWM
+
+# Define the LED pin
+led = Pin(2, Pin.OUT)
+# Attach PWM object on the LED pin and set frequency to 1 kHz
+led_with_pwm = PWM(led, freq=1000)
+led_with_pwm.duty(10)  # approx. 1% duty cycle
+
+# Timer interrupt variables
+timer_counter = 0
+fade_direction = 1  # 1 for increasing brightness, -1 for decreasing
+
+
+def irq_timer0(t):
+    global fade_direction
+    global timer_counter
+
+    # Read currect duty cycle in the range of 1 to 1024 (1-100%)
+    # Note that, pulse width resolution is 10-bit only !
+    current_duty = led_with_pwm.duty()
+
+    # Increment or decrement the PWM duty cycle
+    if fade_direction == 1:
+        new_duty = current_duty + 10
+    else:
+        new_duty = current_duty - 10
+
+    # Change the fade direction when the duty cycle reaches 0 or 1023
+    if new_duty < 0:
+        new_duty = 0
+        fade_direction = 1
+    elif new_duty > 1023:
+        new_duty = 1020
+        fade_direction = 0
+
+    # Update the duty cycle in the range of 1 to 100
+    led_with_pwm.duty(new_duty)
+    # print(f"dir:{fade_direction}, cur:{current_duty}, new:{new_duty}")
+
+    timer_counter += 1
+
+
+# Create a Timer0 object for the interrupt
+timer0 = Timer(0)
+timer0.init(mode=Timer.PERIODIC, period=15, callback=irq_timer0)
+
+print("Stop the code execution by pressing `Ctrl+C` key.")
+print("If it does not respond, press the onboard `reset` button.")
+print("")
+print("Start fading for approx. 10 secs...")
+   
+# Main loop to run the LED fading
+while True:
+    if timer_counter >= 700:  # Run for approximately 10 seconds
+        timer0.deinit()  # Deinitialize the timer
+        led_with_pwm.duty(0)  # Turn off the LED
+        break
