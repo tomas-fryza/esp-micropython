3f5041107d1679836e800783591bd298ebe422dc
Tomas Fryza <tomas.fryza@gmail.com>
2023-11-16 16:27:19 +0100

    NTP simplified; NOT finished

--- solutions/07-wifi/04-wifi-ntp.py
+++ solutions/07-wifi/04-wifi-ntp.py
@@ -1,76 +1,65 @@
-
-from machine import RTC
 import network
+import mywifi
 import ntptime
-from time import localtime, sleep
-
-
-def connect_wifi():
-    from time import sleep_ms
-
-    if not sta_if.isconnected():
-        print("Connecting to Wi-Fi", end="")
-
-        # Activate station/Wi-Fi client interface
-        sta_if.active(True)
-
-        # Connect
-        sta_if.connect(WIFI_SSID, WIFI_PSWD)
-
-        # Wait untill the connection is estalished
-        while not sta_if.isconnected():
-            print(".", end="")
-            sleep_ms(100)
-
-        print(" Connected")
-
-
-def disconnect_wifi():
-    if sta_if.active():
-        sta_if.active(False)
-
-    if not sta_if.isconnected():
-        print("Disconnected")
-
+from machine import RTC
+import time
 
 # Network settings
-WIFI_SSID = "<YOUR WIFI SSID>"
-WIFI_PSWD = "<YOUR WIFI PASSWORD>"
+# WIFI_SSID = "<YOUR WIFI SSID>"
+# WIFI_PSWD = "<YOUR WIFI PASSWORD>"
 UTC_OFFSET = 1  # CEST is UTC+2:00
                 # CET is UTC+1:00
 
 # Create an independent clock object
 rtc = RTC()
 
-# Create Station interface
-sta_if = network.WLAN(network.STA_IF)
-connect_wifi()
+# Print time after power on
+print(f"RTC time: {rtc.datetime()}")
 
-# Get UTC time from NTP server (pool.ntp.org) and store it
-# to internal RTC
+# Create Station interface
+wifi = network.WLAN(network.STA_IF)
+mywifi.connect(wifi, WIFI_SSID, WIFI_PSWD)
+
+# Get UTC time from NTP server (pool.ntp.org) and set it
+# to the internal RTC
 ntptime.settime()
+print("Local RTC synchronized")
+mywifi.disconnect(wifi)
+
+# Print time after NTP update
+print(f"RTC time: {rtc.datetime()}")
+(year, month, day, wday, hrs, mins, secs, subsecs) = rtc.datetime()
+# Print it in more friendly way
+print(f"{year}-{month}-{day} {hrs}:{mins}:{secs}")
+
+
+
+# TODO: Apply time zone
+
+
 
 # Display UTC (Coordinated Universal Time / Temps Universel Coordonné)
-(year, month, day, wday, hrs, mins, secs, subsecs) = rtc.datetime()
-print(f"UTC Time: {year}-{month}-{day} {hrs}:{mins}:{secs}")
+# (year, month, day, wday, hrs, mins, secs, subsecs) = rtc.datetime()
+# print(f"UTC Time: {year}-{month}-{day} {hrs}:{mins}:{secs}")
 
 # Get epoch time in seconds (for timezone update)
-sec = ntptime.time()
+# sec = ntptime.time()
 
-disconnect_wifi()
 
 # Update your epoch time in seconds and store in to internal RTC
-sec = int(sec + UTC_OFFSET * 60 * 60)
-(year, month, day, hrs, mins, secs, wday, yday) = localtime(sec)
-rtc.datetime((year, month, day, wday, hrs, mins, secs, 0))
+# sec = int(sec + UTC_OFFSET * 60 * 60)
+# (year, month, day, hrs, mins, secs, wday, yday) = time.localtime(sec)
+# rtc.datetime((year, month, day, wday, hrs, mins, secs, 0))
 
-print(f"Local RTC time: UTC+{UTC_OFFSET}:00")
+# print(f"Local RTC time: UTC+{UTC_OFFSET}:00")
 
-# Forever loop
-while True:
-    # Read values from internal RTC
-    (year, month, day, wday, hrs, mins, secs, subsecs) = rtc.datetime()
-    print(f"{year}-{month}-{day} {hrs}:{mins}:{secs}")
+try:
+    while True:
+        print(f"RTC time: {rtc.datetime()}")
+        (year, month, day, wday, hrs, mins, secs, subsecs) = rtc.datetime()
+        print(f"{year}-{month}-{day} {hrs}:{mins}:{secs}")
+        time.sleep(2)
 
-    # Delay 30 seconds
-    sleep(2)
+except KeyboardInterrupt:
+    print("Ctrl+C pressed. Exiting...")
+    mywifi.disconnect(wifi)
