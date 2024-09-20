05565a0e85ed20d6721bbb92ef2bb95c6a2c04e4
tomas-fryza <tomas.fryza@gmail.com>
2024-09-20 21:03:05 +0200

    text position moved

--- examples/91-pico_inky/main.py
+++ examples/91-pico_inky/main.py
@@ -3,19 +3,26 @@
 # https://allrite.blog/2022/09/12/pico-rss-news-feed-reader/
 # https://www.youtube.com/watch?v=ytnBCw5TO9s&ab_channel=MakingStuffwithChrisDeHut
 
-
 import time
 from pimoroni import Button
 from picographics import PicoGraphics
 from picographics import DISPLAY_INKY_PACK # 296x128 mono e-ink
 from picographics import PEN_1BIT
 
-# Create a display object from the class and configure
-display = PicoGraphics(display=DISPLAY_INKY_PACK,
-                       pen_type=PEN_1BIT,
-                       rotate=0)
 
-display.set_backlight(0.5)
+def clear():
+    display.set_pen(WHITE)
+    display.clear()
+    display.update()
+
+
+# Create a display object from the class and configure
+display = PicoGraphics(
+    display=DISPLAY_INKY_PACK,
+    pen_type=PEN_1BIT,
+    rotate=0)
+
+# display.set_backlight(0.5)
 display.set_font("bitmap8")
 
 button_a = Button(12)
@@ -25,24 +32,16 @@ button_c = Button(14)
 WHITE = 15
 BLACK = 0
 
-
-def clear():
-    display.set_pen(WHITE)
-    display.clear()
-    display.update()
-
-
 clear()
 
 display.set_pen(BLACK)
-display.text("Tomas", 5, 5, 240, 7)
-# display.line(5, 56, 291, 56, 1)
-display.text("Fryza", 219, 40, 60, 2)
-display.text("Brno Univ. of Technology", 61, 80, 240, 2)
+display.text("Tomas", 3, 3, 240, 8)
+display.text('"Snowman" Fryza', 35, 67, 200, 2)
+display.text("Brno University of Technology", 152, 94, 200, 1)
 # display.line(10, 100, 286, 100)
 display.set_font("bitmap6")
-display.text("Czechia", 152, 100, 100, 4)
-# display.text("Czechoslovakia", 2, 100, 296, 4)
+display.text("Czechia", 155, 102, 100, 4)
+# display.text("Czechoslovakia", 2, 102, 296, 4)
 
 display.update()
 
