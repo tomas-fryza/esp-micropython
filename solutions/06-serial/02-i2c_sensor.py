40d5e929c92f961a9d78d59e800aed571a048d10
tomas-fryza <tomas.fryza@gmail.com>
2024-10-24 22:19:30 +0200

    variable renamed

--- solutions/06-serial/02-i2c_sensor.py
+++ solutions/06-serial/02-i2c_sensor.py
@@ -11,7 +11,7 @@ Components:
 Authors: MicroPython, https://github.com/micropython/micropython/blob/master/examples/accel_i2c.py
          Tomas Fryza
 Creation Date: 2023-06-17
-Last Modified: 2024-10-22
+Last Modified: 2024-10-24
 """
 
 from machine import I2C
@@ -19,7 +19,7 @@ from machine import Pin
 import time
 import sys
 
-SENSOR_ADDR = 0x5c
+SENSOR_ADDR = 0x5c  # DHT12
 
 # Init I2C using pins GP22 & GP21 (default I2C0 pins)
 i2c = I2C(0, scl=Pin(22), sda=Pin(21), freq=400_000)
@@ -31,16 +31,17 @@ if SENSOR_ADDR not in addrs:
 
 print(f"I2C configuration : {str(i2c)}")
 print("Start using I2C. Press `Ctrl+C` to stop")
+print("")
 print("humidity\t temperature\t checksum")
 
 try:
     # Forever loop
     while True:
         # Read 2 bytes from `SENSOR_ADDR` device, starting at address 0
-        val = i2c.readfrom_mem(SENSOR_ADDR, 0, 5)
-        print(f"{val[0]}.{val[1]} %\t\t {val[2]}.{val[3]} C\t\t {val[4]}")
+        a = i2c.readfrom_mem(SENSOR_ADDR, 0, 5)
+        print(f"{a[0]}.{a[1]} %\t\t {a[2]}.{a[3]} C\t\t {a[4]}")
 
-        if (val[0]+val[1]+val[2]+val[3]) & 0xff != val[4]:
+        if (a[0]+a[1]+a[2]+a[3]) & 0xff != a[4]:
             raise Exception("Checksum error")
         time.sleep(5)
 
