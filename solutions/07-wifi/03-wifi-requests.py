49891a2f562c11c2c7fdbbbe107d6493f2558352
tomas-fryza <tomas.fryza@gmail.com>
2024-10-26 15:56:59 +0200

    request example created

--- solutions/07-wifi/03-wifi-requests.py
+++ solutions/07-wifi/03-wifi-requests.py
@@ -1,18 +1,87 @@
-https://v2.jokeapi.dev/joke/Programming
+"""
+Example of GET/POST requests
 
-https://catfact.ninja/fact
+This script demonstrates how to make GET and POST requests
+using MicroPython. It connects to a specified Wi-Fi network
+and retrieves data from various APIs.
 
-The CoinGecko API's simple/price endpoint allows you to fetch the current price of cryptocurrencies in various fiat currencies.
-https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=eur
+APIs used:
+  - Joke API: https://v2.jokeapi.dev/
+  - Cat Fact API: https://catfact.ninja/
+  - Time API: https://timeapi.io/
+  - CoinGecko API: https://coingecko.com/
 
-https://timeapi.io/swagger/index.html
-https://timeapi.io/api/time/current/zone?timeZone=Europe/Prague
+Other APIs (api-key needed):
+   https://quotes.rest/
+   https://openweathermap.org/
+   https://openaq.org/
+   https://www.iqair.com/world-air-quality
 
-https://restcountries.com/
-https://restcountries.com/v3.1/name/czechia
+Components:
+  - ESP32 microcontroller
+
+Authors: Tomas Fryza
+Creation Date: 2024-10-25
+Last Modified: 2024-10-26
+"""
+
+import network
+import my_wifi
+import config
+import urequests  # Network Request Module
+import time
+
+# Create Station interface
+wifi = network.WLAN(network.STA_IF)
+print("Start using Wi-Fi. Press `Ctrl+C` to stop")
+
+try:
+    # Forever loop
+    while True:
+        my_wifi.connect(wifi, config.SSID, config.PSWD)
 
 
-https://quotes.rest/
-https://openweathermap.org/
-https://openaq.org/
-https://www.iqair.com/world-air-quality
+        # GET requests
+        API_URL = "https://catfact.ninja/fact"
+
+        # API_URL = "https://v2.jokeapi.dev/joke/Programming"
+
+        # List of GET/POST methods: https://timeapi.io/swagger/index.html
+        # API_URL = "https://timeapi.io/api/time/current/zone?timeZone=Europe/Prague"
+
+        # Fetch the current price of cryptocurrencies in various currencies
+        # https://docs.coingecko.com/reference/simple-price
+        # API_URL = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=eur"
+
+        print("Method used: GET")
+        request_url = f"{API_URL}"
+        response = urequests.get(request_url)
+
+
+        # POST requests
+        # List of GET/POST methods: https://timeapi.io/swagger/index.html
+        # API_URL = "https://timeapi.io/api/conversion/converttimezone"
+
+        # print("Method used: POST")
+        # request_url = f"{API_URL}"
+        # json = {"fromTimeZone": "Europe/Prague",
+        #         "dateTime": "2024-10-26 15:33:00",
+        #         "toTimeZone": "America/Los_Angeles",
+        #         "dstAmbiguity": ""}
+        # headers = {"Content-Type": "application/json"}
+        # response = urequests.post(request_url, json=json, headers=headers)
+
+
+        print("Response:")
+        print(response.text)
+        response.close()
+
+        my_wifi.disconnect(wifi)
+        time.sleep(30)
+
+except KeyboardInterrupt:
+    # This part runs when Ctrl+C is pressed
+    print("Program stopped. Exiting...")
+
+    # Optional cleanup code
+    my_wifi.disconnect(wifi)
