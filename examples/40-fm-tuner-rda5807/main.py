b53f06885ad5a42406b9cbcc921c92a7ed11d57e
tomas-fryza <tomas.fryza@gmail.com>
2025-03-05 10:54:43 +0100

    mutu button interrupt added

--- examples/40-fm-tuner-rda5807/main.py
+++ examples/40-fm-tuner-rda5807/main.py
@@ -1,13 +1,29 @@
+"""
+FM radio & sensors
 
-# https://101-things.readthedocs.io/en/latest/fm_radio.html
-# https://github.com/wagiminator/ATtiny85-TinyFMRadio/blob/master/software/TinyFMRadio.ino
-# https://github.com/pu2clr/RDA5807/tree/master/examples
-# https://maker.pro/arduino/projects/simple-fm-radio-receiver-with-arduino-uno-and-rda5807m
+This MicroPython script integrates an OLED display, FM radio module,
+rotary encoder, PIR motion sensor, and BMP180 pressure sensor. Key
+functionalities include controlling the FM radio's volume and frequency,
+displaying temperature and pressure data, motion detection with the PIR
+sensor, and updating the OLED display with real-time sensor readings.
 
-# https://github.com/franckinux/python-rd5807m/tree/master
+Authors:
+- Tomas Fryza
+- Ondrej Kolar
 
-# MicroPython TechNotes: Rotary Encoder
-# https://techtotinker.com/2021/04/13/027-micropython-technotes-rotary-encoder/
+Creation date: 2025-01-23
+Last modified: 2025-03-05
+
+Inspired by:
+FM RDA5807M
+  * https://101-things.readthedocs.io/en/latest/fm_radio.html
+  * https://github.com/franckinux/python-rd5807m/tree/master
+  * https://github.com/wagiminator/ATtiny85-TinyFMRadio/blob/master/software/TinyFMRadio.ino
+  * https://github.com/pu2clr/RDA5807/tree/master/examples
+
+Rotary Encoder
+  * https://techtotinker.com/2021/04/13/027-micropython-technotes-rotary-encoder/
+"""
 
 # Micropython builtin modules
 import time
@@ -20,15 +36,27 @@ import rda5807                    # FM radio module
 from rotary_irq import RotaryIRQ  # Rotary encoder
 
 LED_PIN = 2
-BUZZ_PIN = 13
-PIR_PIN = 15
 BTN_UP_PIN = 19
 BTN_DOWN_PIN = 18
 ROT_PIN = 33
 ROT_CLK_PIN = 32
 ROT_DT_PIN = 35
+# BUZZ_PIN = 13
+PIR_PIN = 15
 
-i2c = SoftI2C(sda=Pin(21), scl=Pin(22), freq=400_000)
+
+def button_mute(pin):
+    global led, mute
+
+    led.on()
+    time.sleep_ms(20)
+    mute = not mute
+    radio.mute(mute)
+    print(f"Mute: {mute}")
+
+
+# Init I2C using pins 22 & 21 (default I2C pins)
+i2c = SoftI2C(sda=Pin(21), scl=Pin(22))  #, freq=400_000)
 print(f"I2C configuration : {str(i2c)}")
 
 # I2C devices
@@ -43,11 +71,14 @@ btn_up = Pin(BTN_UP_PIN, Pin.IN, Pin.PULL_UP)
 btn_down = Pin(BTN_DOWN_PIN, Pin.IN, Pin.PULL_UP)
 btn_rot = Pin(ROT_PIN, Pin.IN, Pin.PULL_UP)
 
+# Attach interrupt to the button pin (trigger on falling edge)
+btn_rot.irq(trigger=Pin.IRQ_FALLING, handler=button_mute)
+
 # Other devices
-# buzzer = PWM(Pin(BUZZ_PIN, Pin.OUT), duty=0) # duty=0 prevents default waveform from starting immediately
+# buzzer = PWM(Pin(BUZZ_PIN, Pin.OUT), duty=0)  # duty=0 prevents default waveform from starting immediately
 pir = Pin(PIR_PIN, Pin.IN)
 rot = RotaryIRQ(pin_num_clk=ROT_CLK_PIN,
-                pin_num_dt=ROT_DL_PIN,
+                pin_num_dt=ROT_DT_PIN,
                 min_val=0,
                 max_val=15,
                 pull_up=False,
@@ -58,11 +89,12 @@ rot = RotaryIRQ(pin_num_clk=ROT_CLK_PIN,
 # Set FM module
 vol = rot.value()
 mute = False
-radio.set_volume(vol)           # 0-15
+radio_text = ""
+radio.set_volume(vol)           # 0--15
 radio.set_frequency_MHz(103.4)  # 103.4 - Blanik
-# radio.mute(mute)
+radio.mute(mute)
 
-print("Start using FM module and sensors. Press `Ctrl+C` to stop")
+print("\nStart using FM module and sensors. Press `Ctrl+C` to stop")
 
 try:
     # Forever loop
@@ -70,27 +102,21 @@ try:
         # Clear display
         display.fill(0)
 
-        # Volume
+        # Volume 0--15
         vol_new = rot.value()
         if vol != vol_new:
             vol = vol_new
-            radio.set_volume(vol) # 0-15
+            radio.set_volume(vol)
+            print(f"Volume: {vol}")
         display.text('vol: '+str(vol), 0, 16, 1)
 
         # Buttons: seek frequency up, down, and mute
-        if (btn_grn.value() == 0):
+        if (btn_up.value() == 0):
             led.on()
             radio.seek_up()
-            led.off()
-        elif (btn_red.value() == 0):
+        elif (btn_down.value() == 0):
             led.on()
             radio.seek_down()
-            led.off()
-        elif (btn_rot.value() == 0):
-            led.on()
-            mute = not mute
-            radio.mute(True)
-            led.off()
         else:
             led.off()
 
@@ -108,8 +134,10 @@ try:
         radio_name = "".join(map(str, radio.station_name))
         display.text(str(radio_name), 0, 0, 1)
 
-        radio_text = "".join(map(str, radio.radio_text))
-        print(f"RDS text: {radio_text}")
+        radio_text_new = "".join(map(str, radio.radio_text))
+        if radio_text != radio_text_new:
+            radio_text = radio_text_new
+            print(f"RDS text: {radio_text}")
 
         display.text(str(radio.get_frequency_MHz())+' MHz', 0, 8, 1)
 
@@ -117,7 +145,7 @@ try:
         display.text(str(rssi)+' dBm', 85, 8, 1)
 
         display.show()
-        time.sleep_ms(5)
+        time.sleep_ms(20)
 
 except KeyboardInterrupt:
     # This part runs when Ctrl+C is pressed
@@ -125,4 +153,4 @@ except KeyboardInterrupt:
 
     # Optional cleanup code
     led.off()
-    buzzer.deinit()
+    # buzzer.deinit()
