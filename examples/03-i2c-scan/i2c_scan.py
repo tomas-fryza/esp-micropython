d664a80bc100b0cbcab6eab0c439dd95bc0dcd85
tomas-fryza <tomas.fryza@gmail.com>
2025-04-07 13:54:29 +0200

    i2c scan example added

---
+++ examples/03-i2c-scan/i2c_scan.py
@@ -0,0 +1,28 @@
+"""
+I2C scanner
+
+Scan the I2C (Inter-Integrated Circuit) bus for connected
+devices and print their addresses. This script is useful
+for identifying I2C devices connected to your microcontroller.
+
+Authors:
+- MicroPython
+- Tomas Fryza
+
+Creation date: 2023-06-17
+Last modified: 2024-11-02
+"""
+
+from machine import I2C
+from machine import Pin
+
+# Init I2C using pins GP22 & GP21 (default I2C0 pins)
+i2c = I2C(0, scl=Pin(22), sda=Pin(21), freq=100_000)
+
+print("Scanning I2C... ", end="")
+addrs = i2c.scan()
+print(f"{len(addrs)} device(s) detected")
+
+print("dec.\t hex.")
+for addr in addrs:
+    print(f"{addr}\t {hex(addr)}")
